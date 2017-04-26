6698e43ffd8ea1782a827c33a8a1a7bffa375e31
ewhite7 <edw@rdjam.es>
2017-04-26 23:30:04 +0000

    updated email address

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,5 +1,5 @@
 class ContactMailer < ActionMailer::Base
-  default to: 'edward@jam.es'
+  default to: 'ej@drinkjavazen.com'
   def contact_email(name, email, body)
     @name = name
     @email = email
